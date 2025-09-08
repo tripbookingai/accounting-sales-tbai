71748e924a016a5ceebba10068018747d047c52a
nehaltrio <ahmed.nehal@northsouth.edu>
2025-09-08 16:32:32 +0600

    build error fixed

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -2,11 +2,6 @@ import { NextRequest, NextResponse } from 'next/server'
 import path from 'path'
 import fs from 'fs/promises'
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-}
 
 export async function POST(req: NextRequest) {
   const formData = await req.formData()
